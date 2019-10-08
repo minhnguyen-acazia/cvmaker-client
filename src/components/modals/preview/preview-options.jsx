85f7072d9561c0e8ce20831d6d84a063eee9199b
minh nguyen <nguyentruongminh0604@gmail.com>
2019-10-08 13:51:30 +0700

    Adding resume components

--- src/components/modals/preview/preview-options.jsx
+++ src/components/modals/preview/preview-options.jsx
@@ -1,36 +1,69 @@
 import React from 'react'
 import './preview-options.scss'
 import { ModalBlueprint } from '../modal-blueprint/modal-blueprint'
+import PreviewElegantModern from '../../../assets/images/preview-elegant-modern.png'
+import PreviewExperience from '../../../assets/images/preview-experience.png'
+import PreviewMinimalAndClean from  '../../../assets/images/preview-minimal-and-clean.png'
+import PreviewSimpleAndClean from '../../../assets/images/preview-simple-and-clean.png'
 
-const options = [
-  {
-    name: 'Elegant Modern'
-  },
-  {
-    name: 'Experience'
-  },
-  {
-    name: 'Minimal and Clean'
-  },
-  {
-    name: 'Simple and Clean'
+export class PreviewOptions extends React.Component {
+  state = {
+    options: [
+      {
+        name: 'Elegant Modern',
+        img: PreviewElegantModern,
+        selected: false
+      },
+      {
+        name: 'Experience',
+        img: PreviewExperience,
+        selected: false
+      },
+      {
+        name: 'Minimal and Clean',
+        img: PreviewMinimalAndClean,
+        selected: false
+      },
+      {
+        name: 'Simple and Clean',
+        img: PreviewSimpleAndClean,
+        selected: false
+      }
+    ]
   }
-]
 
-export function PreviewOptions() {
-  return (
-    <ModalBlueprint className='preview-options' onConfirm={() => {}}>
-        Style
-        <div style={{clear: 'both'}} />
-        <div className='options'>
-          {options.map(n => (
-            <p>
-              <label>{n.name}</label>
-              <input type='radio' value='test' />
-            </p>
-          ))}
-        </div>
-        <div className='preview-thumb'>img</div>
-    </ModalBlueprint>
-  )
+  onSelectOption = (idx) => {
+    const { options } = this.state
+
+    options.forEach((n, index) => {
+      n.selected = false
+      if (index === idx) n.selected = true 
+    })
+
+    this.setState({ options })
+  }
+
+  render() {
+    const { options } = this.state
+
+    return (
+      <ModalBlueprint className='preview-options' onConfirm={() => {}}>
+          Style
+          <div style={{clear: 'both'}} />
+          <div className='options'>
+            {options.map((n, idx) => (
+              <p>
+                <label>{n.name}</label>
+                <input type='radio' value='test' checked={n.selected} onChange={() => this.onSelectOption(idx)} />
+              </p>
+            ))}
+          </div>
+          <div className='preview-thumb'>
+            {options.find(n => n.selected)
+            ? <img src={options.find(n => n.selected).img} alt='preview' />
+            : ''}
+          </div>
+      </ModalBlueprint>
+    )
+  }
 }
---
+++ src/components/resumes/resume-experience/resume-experiece.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function ResumeExperience() {
+	return (
+		<div className='resume-experience'>
+			<div className='left-column'>
+				<div className='photo'>
+					
+				</div>
+			</div>
+			<div className='right-column'>
+
+			</div>
+		</div>
+	)
+}
