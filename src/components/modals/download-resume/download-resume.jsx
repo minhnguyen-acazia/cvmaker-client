7178b7af36207d5a37c91f3eff5cf57887eba490
minh nguyen <nguyentruongminh0604@gmail.com>
2019-10-11 11:08:04 +0700

    Added spinner when loading for resume

---
+++ src/components/modals/download-resume/download-resume.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function DownloadResume() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- src/components/modals/preview/preview-resume.jsx
+++ src/components/modals/preview/preview-resume.jsx
@@ -1,27 +1,36 @@
 import React, { useEffect } from 'react'
 import html2canvas from 'html2canvas'
+import Spinner from 'react-spinkit'
 import './preview-resume.scss'
 import { ModalBlueprint } from '../modal-blueprint/modal-blueprint'
 
 export function PreviewResume({ children }) {
   useEffect(() => {
-    const resume = document.querySelector('.resume-experience')
+    const resume = document.querySelector('.mock-data').children[0]
     html2canvas(resume).then(canvas => {
       // remove old element
-      resume.remove()
+      document.querySelector('.mock-data').remove()
       // create an image of the canvas
       const imgData = canvas.toDataURL('image/png')
       const img = new Image()
       img.src = imgData
       img.classList.add('resume')
       // append that image
+      document.querySelector('.place-holder').remove()
       document.querySelector('.modal-blueprint__content').appendChild(img)
     })
   }, [])
 
   return (
-    <ModalBlueprint className='preview-resume'>
-      {children}
-    </ModalBlueprint>
+    <>
+      <div className='mock-data'>
+        {children}
+      </div>
+      <ModalBlueprint className='preview-resume'>
+        <div className='place-holder'>
+          <Spinner name='ball-spin-fade-loader' color='#07A9C5' />
+        </div>
+      </ModalBlueprint>
+    </>
   )
 }
