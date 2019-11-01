87a1070ebc0cf3ec49ecb89d499a7e27e8471c01
minh nguyen <nguyentruongminh0604@gmail.com>
2019-11-01 18:39:24 +0700

    Creating resume elegant modern

--- src/app-loader.js
+++ src/app-loader.js
@@ -6,6 +6,7 @@ import { ModalRegistry } from './components/modals/modal-registry'
 import { CreateCVGuest } from './routes/guest/create-cv-guest/create-cv-guest'
 import { ResumeExperience } from './components/resumes/resume-experience/resume-experience'
 import { ResumeSimpleAndClean } from './components/resumes/resume-simple-and-clean/resume-simple-and-clean'
+import { ResumeElegantModern } from './components/resumes/resume-elegant-modern/resume-elegant-modern'
 
 export default class App extends Component {
   render() {
@@ -16,6 +17,7 @@ export default class App extends Component {
             <Route exact path='/' component={CreateCVGuest} />
             <Route exact path='/resume-experience' component={ResumeExperience} />
             <Route exact path='/resume-simple-and-clean' component={ResumeSimpleAndClean} />
+            <Route exact path='/resume-elegant-modern' component={ResumeElegantModern} />
           </Switch>
         </Router>
         <ModalRegistry />
---
+++ src/components/resumes/resume-elegant-modern/resume-elegant-modern.jsx
@@ -0,0 +1,153 @@
+import React from 'react'
+import './resume-elegant-modern.scss'
+import avatar from '../../../assets/images/resume-elegant-modern/avatar.png'
+
+export function ResumeElegantModern() {
+  return (
+    <div className='resume-elegant-modern'>
+      <div className='section-1'>
+        <div className='col-1'>
+					<div className='avatar'>
+            <img src={avatar} alt='avatar' />
+          </div>
+          <div className='profile'>
+            <div className='name'>
+              <span className='first-name'>Paul</span>
+              <br />
+              <span className='last-name'>Waulson</span>
+            </div>
+            <div className='title'>Web &amp; Graphic Designer</div>
+          </div>
+        </div>
+        <div className='col-2'>
+          <div className='contact'>
+            <div className='contact-item'>
+              <span className='label'>Phone:</span>
+              <span className='value'>+00 1234 567 89</span>
+            </div>
+            <div className='contact-item'>
+              <span className='label'>Area:</span>
+              <span className='value'>123, street, New York, USA</span>
+            </div>
+            <div className='contact-item'>
+              <span className='label'>Email:</span>
+              <span className='value'>Free@PSDFreebies.com</span>
+            </div>
+          </div>
+          <div className='about'>
+            <div className='title'>ABOUT ME</div>
+            <div className='description'>Porttitor amet massa Done cporttitor dolor et nisl lorem ipsum dolor molaternpretium iscon fringilla delislibero lorem ipsum sollicitudin est prttitor.</div>
+          </div>
+        </div>
+      </div>
+
+      <div className='section-2'>
+        <div className='experiences'>
+          <div className='header'>
+            <div className='header-title'>EXPERIENCE</div>
+            <div className='header-line'/>
+          </div>
+          <div className='experience'>
+            <div className='col-1'>
+              <div className='title'>SENIOR WEB DEVELOPER</div>
+              <div className='time'>Jan 2017 - Dec 2015</div>
+            </div>
+            <div className='col-2'>
+              <div className='place'>Works in Lorem Ipsum - United States</div>
+              <div className='description'>Porttitor amet massa Done cporttitor dolor et nisl lorem ipsum dolor molaternpretium iscon fringilla delislibero lorem ipsum sollicitudin est prttitor.</div>
+            </div>
+          </div>
+          <div className='experience'>
+            <div className='col-1'>
+              <div className='title'>SENIOR WEB DEVELOPER</div>
+              <div className='time'>Mar 2011 - Feb 2015</div>
+            </div>
+            <div className='col-2'>
+              <div className='place'>Works in Lorem Ipsum - New York</div>
+              <div className='description'>Porttitor amet massa Done cporttitor dolor et nisl lorem ipsum dolor molaternpretium iscon fringilla delislibero lorem ipsum sollicitudin est prttitor.</div>
+            </div>
+          </div>
+          <div className='experience'>
+            <div className='col-1'>
+              <div className='title'>SENIOR WEB DEVELOPER</div>
+              <div className='time'>Mar 2009 - Feb 2011</div>
+            </div>
+            <div className='col-2'>
+              <div className='place'>Works in Lorem Ipsum - New York</div>
+              <div className='description'>Porttitor amet massa Done cporttitor dolor et nisl lorem ipsum dolor molaternpretium iscon fringilla delislibero lorem ipsum sollicitudin est prttitor. ipsum dolor molaternpretium iscon fringilla delislibero lorem ipsum sollicitudin est prttitor.</div>
+            </div>
+          </div>
+        </div>
+        <div className='skills'>
+          <div className='pro-skills'>
+            <div className='header'>
+              <div className='header-title'>PRO SKILLS</div>
+              <div className='header-line'/>
+            </div>
+            <div className='skill'>
+              <div className='skill-name'>Photoshop</div>
+              <div className='skill-percentage'>
+                <div className='actual' />
+              </div>
+            </div>
+            <div className='skill'>
+              <div className='skill-name'>Illustator</div>
+              <div className='skill-percentage'>
+                <div className='actual' />
+              </div>
+            </div>
+            <div className='skill'>
+              <div className='skill-name'>Javascript</div>
+              <div className='skill-percentage'>
+                <div className='actual' />
+              </div>
+            </div>
+            <div className='skill'>
+              <div className='skill-name'>HTML / CSS</div>
+              <div className='skill-percentage'>
+                <div className='actual' />
+              </div>
+            </div>
+          </div>
+          <div className='per-skills'>
+            <div className='header'>
+              <div className='header-title'>PER SKILLS</div>
+              <div className='header-line'/>
+            </div>
+            <div className='skill'>
+              <div className='skill-name'>Creativity</div>
+              <div className='skill-percentage'>
+                <div className='actual' />
+              </div>
+            </div>
+            <div className='skill'>
+              <div className='skill-name'>Teamwork</div>
+              <div className='skill-percentage'>
+                <div className='actual' />
+              </div>
+            </div>
+            <div className='skill'>
+              <div className='skill-name'>Hardwork</div>
+              <div className='skill-percentage'>
+                <div className='actual' />
+              </div>
+            </div>
+            <div className='skill'>
+              <div className='skill-name'>Leadership</div>
+              <div className='skill-percentage'>
+                <div className='actual' />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='section-3'>
+
+      </div>
+      <div className='section-4'>
+
+      </div>
+    </div>
+  )
+}
